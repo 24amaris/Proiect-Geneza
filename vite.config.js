ef1bf6df57f2853d3fbb5563ad2fc6ded602043f
amarispurice <amaris.purice@emmanuel.ro>
2024-11-22 11:32:52 +0200

    am adaugat paginile

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html", "./predici.html","./rugaciune.html","./despreNoi.html","./evenimente.html",]
+      input: [ "./index.html", "./predici.html","./rugaciune.html","./despreNoi.html","./evenimente.html","./card.html","./comunitate.html","./contact.html","./departamente.html","./doneaza.html","./transfer.html","./verset.html"]
     }
   }
 })
